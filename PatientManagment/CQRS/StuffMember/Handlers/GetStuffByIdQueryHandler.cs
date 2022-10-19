deb2ddaa53451cdd38845704c9e6773e175e28b2
Mateusz Szreder <mateusz.szreder00@gmail.com>
2022-10-19 22:32:53 +0200

    merge from add by id api method

---
+++ PatientManagment/CQRS/StuffMember/Handlers/GetStuffByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using PatientManagement.Core.CQRS.StuffMember.Queries;
+using PatientManagement.Core.CQRS.StuffMember.Responses;
+using PatientManagement.Core.Interfaces;
+
+namespace PatientManagement.Core.CQRS.StuffMember.Handlers
+{
+    internal class GetStuffByIdQueryHandler : IRequestHandler<GetStuffMemberByIdQuery, StuffMemberResponse>
+    {
+        private readonly IGenericRepository<Entities.StuffMember> _stuffMemberRepository;
+        private readonly IMapper _mapper;
+        public GetStuffByIdQueryHandler(IGenericRepository<Entities.StuffMember> repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _stuffMemberRepository = repo;
+        }
+
+        public async Task<StuffMemberResponse> Handle(GetStuffMemberByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var stuffMember = _stuffMemberRepository
+                .GetWithEntity(x => x.Department, x => x.TypeOfStuffMember)
+                .FirstOrDefault(c => c.Id == request.Id);
+
+            return _mapper.Map<StuffMemberResponse>(stuffMember);
+        }
+    }
+}
