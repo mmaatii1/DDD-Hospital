10b9c1d4ed33240b005a851de6ece46e4680160d
Mateusz Szreder <mateusz.szreder00@gmail.com>
2022-10-30 14:20:39 +0100

    add working modal !!!!!!

--- HospitalNG/src/app/shared/modal/modal.component.ts
+++ HospitalNG/src/app/shared/modal/modal.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
+	selector: 'modal-component',
+	templateUrl: './modal.component.html',
 })
-export class ModalComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() { }
+export class ModalComponent {
+	closeResult = '';
 
-  displayStyle = "none";
-  
-  openPopup() {
-    this.displayStyle = "block";
-  }
-  closePopup() {
-    this.displayStyle = "none";
-  }
+	constructor(private modalService: NgbModal) {}
+
+	open(content: any) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal.component-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+	private getDismissReason(reason: any): string {
+		if (reason === ModalDismissReasons.ESC) {
+			return 'by pressing ESC';
+		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+			return 'by clicking on a backdrop';
+		} else {
+			return `with: ${reason}`;
+		}
+	}
 }
